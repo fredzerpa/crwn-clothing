4b87cf5f8542ff6ba848d1c658c507a8e99275de
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-08-08 23:06:44 -0400

    Updating Files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/Header.component';
 import HomePage from './pages/homepage/HomePage.component';
 import ShopPage from './pages/shop/ShopPage.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInAndSignUpPage.component';
 
@@ -18,13 +18,24 @@ class App extends Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log('Google User', user);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
-
-    console.log('Function Auth', this.unsubscribeFromAuth);
   }
 
   componentWillUnmount() {
@@ -32,10 +43,10 @@ class App extends Component {
   }
 
   render() {
-    const {currentUser} = this.state;
+    const { currentUser } = this.state;
     return (
       <div>
-        <Header currentUser={currentUser}/>
+        <Header currentUser={currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
