45e46d0a757ed89b0cd97b8ab8079ae2e7bc4b5e
TOR <52118178+fredzerpa@users.noreply.github.com>
2020-09-27 03:15:01 -0400

    Updating code to be PWA Compliant, added ServiceWorker, HTTPS, Uptaded Manifest

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.css';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -21,3 +23,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -10,13 +11,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
@@ -31,7 +33,7 @@ app.post('/payment', (req, res) => {
     amount: amount,
     currency: 'usd',
   };
-
+  
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
@@ -40,6 +42,10 @@ app.post('/payment', (req, res) => {
     }
   });
 });
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'build', 'service-worker.js')); 
+})
 
 app.listen(port, error => {
   if (error) throw new Error(error);
